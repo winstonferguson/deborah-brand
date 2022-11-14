5dab355fed83502dfac5d9f612d1f632b800412a
Poshcock <winston@poshcock.com>
2022-11-14 13:16:04 +0000

    image with text sizing

--- assets/section-db-image-with-text.js
+++ assets/section-db-image-with-text.js
@@ -22,15 +22,15 @@ function setUpDBContainers() {
       if (dbContainersLinkBar) {
         let link = document.createElement('a');
         link.setAttribute('href', '#');
-        link.classList.add("button", "db-image-with-text__button" );
-        link.innerHTML =  value.querySelector(".db-image-with-text__heading").innerHTML;
+        link.classList.add("button", "db-image-with-text__button");
+        link.innerHTML = value.querySelector(".db-image-with-text__heading").innerHTML;
         link.addEventListener('click', function (event) {
           event.preventDefault();
           value.scrollIntoView({ behavior: 'smooth' });
         });
 
         dbContainersLinkBar.append(link)
-      } 
+      }
     } else {
       dbButton.addEventListener('click', function (event) {
         event.preventDefault();
@@ -40,21 +40,60 @@ function setUpDBContainers() {
       if (dbContainersLinkBar) {
         let link = document.createElement('a');
         link.setAttribute('href', '#');
-        link.classList.add("button", "db-image-with-text__button" );
-        link.innerHTML =  value.querySelector(".db-image-with-text__heading").innerHTML;
+        link.classList.add("button", "db-image-with-text__button");
+        link.innerHTML = value.querySelector(".db-image-with-text__heading").innerHTML;
         link.addEventListener('click', function (event) {
           event.preventDefault();
           value.scrollIntoView({ behavior: 'smooth' });
         });
 
         dbContainersLinkBar.append(link)
-      }      
+      }
     }
   }
 
   return d3b0rahBContainersActive = true;
 }
 
+function sizeDBContainers() {
+  const isLargeScreen = window.matchMedia("(min-width: 990px)");
+  const dbContainers = document.querySelectorAll(".db-image-with-text__container");
+
+  if (isLargeScreen.matches) {
+    for (const container of dbContainers) {
+      sizeDBContainer(container);
+    }
+  } else {
+    for (const container of dbContainers) {
+      container.style = maxHeight = "none";
+    }
+  }
+}
+
+function DBContainerImageClass(dbcontainer) {
+  const dbImageContent = dbcontainer.querySelector(".db-image-with-text__image");
+  const dbImage = dbcontainer.querySelector("img");
+  const dbImageOrientation = (dbImage.offsetHeight > dbImage.offsetWidth) ? "portrait" : "landscape";
+
+  return dbImageContent.classList.add(dbImageOrientation);
+}
+
+function sizeDBContainer(dbcontainer) {
+  const dbContent = dbcontainer.querySelector(".db-image-with-text__content");
+  const dbContentHeight = dbContent.offsetHeight;
+
+  DBContainerImageClass(dbcontainer);
+
+  return dbcontainer.style.height = dbContentHeight + 400 + "px";
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  if (!d3b0rahBContainersActive) setUpDBContainers();
+  if (!d3b0rahBContainersActive) {
+    setUpDBContainers();
+    sizeDBContainers();
+  }
+});
+
+window.addEventListener('resize', () => {
+  sizeDBContainers();
 });
